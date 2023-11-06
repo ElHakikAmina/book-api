226dde33ac666d137deaf1faecf74cc41bfe2d15
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2023-11-06 21:10:21 +0000

    les annotation pour la config

--- src/main/java/com/bushansirgur/spring/config/AppConfig.java
+++ src/main/java/com/bushansirgur/spring/config/AppConfig.java
@@ -1,10 +1,25 @@
 package com.bushansirgur.spring.config;
-import org.springframework.context.annotation.Configuration;
-
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.context.annotation.*;
 
 
 @Configuration
-
+@PropertySource("classpath:db.properties")
+@EnableTransactionManagement
+@ComponentScans(value = {
+		@ComponentScan("com.bushansirgur.spring.dao"),
+		@ComponentScan("com.bushansirgur.spring.service")
+})
 public class AppConfig {
+	
+	@Bean
+	public LocalSessionFactoryBean getSessionFactory() {
+		
+	}
+	
+	
+	
+	
 
 }
