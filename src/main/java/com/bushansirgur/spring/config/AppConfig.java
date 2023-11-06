d11e6bd51f48e9a933d6db7d2e49c985f4d25ca4
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2023-11-06 21:44:39 +0000

    set JDBC properties

--- src/main/java/com/bushansirgur/spring/config/AppConfig.java
+++ src/main/java/com/bushansirgur/spring/config/AppConfig.java
@@ -6,6 +6,12 @@ import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.hibernate.cfg.Environment.*;
+
+import java.util.Properties;
+
 
 @Configuration
 @PropertySource("classpath:db.properties")
@@ -16,9 +22,37 @@ import org.springframework.context.annotation.Bean;
 })
 public class AppConfig {
 	
+	@Autowired
+	private Environment env;
+	
 	@Bean
 	public LocalSessionFactoryBean getSessionFactory() {
 		LocalSessionFactoryBean factoryBean =  new LocalSessionFactoryBean(); 
+		Properties props = new Properties();
+		
+		props.put(DRIVER,  env.getProperty("mysql.driver"));
+		props.put(URL,  env.getProperty("mysql.url"));
+		props.put(USER,  env.getProperty("mysql.user"));
+		props.put(PASS,  env.getProperty("mysql.password"));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 	
 	
