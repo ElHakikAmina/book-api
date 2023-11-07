7447259bdd0b7fda095f65830b066ed3284bff31
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2023-11-07 00:12:16 +0000

    MyWebAppInitializer class

---
+++ src/main/java/com/bushansirgur/spring/config/MyWebAppInitializer.java
@@ -0,0 +1,24 @@
+package com.bushansirgur.spring.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] {AppConfig.class};
+		
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {WebConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new Class[] {"/"}
+	}
+
+	
+}
