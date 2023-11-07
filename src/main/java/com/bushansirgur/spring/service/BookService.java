fd8bd7aa255df868533d75b9b4c63ae5f519840a
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2023-11-07 01:23:49 +0000

    service

---
+++ src/main/java/com/bushansirgur/spring/service/BookService.java
@@ -0,0 +1,5 @@
+package com.bushansirgur.spring.service;
+
+public interface BookService {
+
+}
---
+++ src/main/java/com/bushansirgur/spring/service/BookServiceImpl.java
@@ -0,0 +1,49 @@
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bushansirgur.spring.dao.BookDAO;
+import com.bushansirgur.spring.model.Book;
+
+
+@Service
+public class BookServiceImpl implements BookService{
+	
+	@Autowired
+	private BookDAO bookDAO;
+
+	@Override
+	public long save(Book book) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Book get(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	@Transactional
+	public List<Book> list() {
+		return bookDAO.list();
+	}
+
+	@Override
+	public void update(long id, Book book) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
